7fd58edbe01e9642de87977f14b92157900df299
Michael Polevoi <polmichael552@gmail.com>
2024-08-22 10:27:53 +0300

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Calculator
+{
+    partial class MyCalculatorMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyCalculatorMain));
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnSquareRoot = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnFactorial = new System.Windows.Forms.Button();
+            this.btnDEL = new System.Windows.Forms.Button();
+            this.btnAC = new System.Windows.Forms.Button();
+            this.btnANS = new System.Windows.Forms.Button();
+            this.btnPowerAny = new System.Windows.Forms.Button();
+            this.btnMultiplication = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnAddition = new System.Windows.Forms.Button();
+            this.btnSubstraction = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.helpBtn = new System.Windows.Forms.Button();
+            this.floatingPtBtn = new System.Windows.Forms.Button();
+            this.ansBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn0, "btn0");
+            this.btn0.Name = "btn0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnSquareRoot
+            // 
+            this.btnSquareRoot.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.btnSquareRoot, "btnSquareRoot");
+            this.btnSquareRoot.Name = "btnSquareRoot";
+            this.btnSquareRoot.UseVisualStyleBackColor = false;
+            this.btnSquareRoot.Click += new System.EventHandler(this.btnSquareRoot_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn8, "btn8");
+            this.btn8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn8.Name = "btn8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btnFactorial
+            // 
+            this.btnFactorial.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.btnFactorial, "btnFactorial");
+            this.btnFactorial.Name = "btnFactorial";
+            this.btnFactorial.UseVisualStyleBackColor = false;
+            this.btnFactorial.Click += new System.EventHandler(this.btnFactorial_Click);
+            // 
+            // btnDEL
+            // 
+            this.btnDEL.BackColor = System.Drawing.Color.Lime;
+            resources.ApplyResources(this.btnDEL, "btnDEL");
+            this.btnDEL.Name = "btnDEL";
+            this.btnDEL.UseVisualStyleBackColor = false;
+            this.btnDEL.Click += new System.EventHandler(this.btnDEL_Click);
+            // 
+            // btnAC
+            // 
+            this.btnAC.BackColor = System.Drawing.Color.Lime;
+            resources.ApplyResources(this.btnAC, "btnAC");
+            this.btnAC.Name = "btnAC";
+            this.btnAC.UseVisualStyleBackColor = false;
+            this.btnAC.Click += new System.EventHandler(this.btnAC_Click);
+            // 
+            // btnANS
+            // 
+            this.btnANS.BackColor = System.Drawing.Color.Lime;
+            resources.ApplyResources(this.btnANS, "btnANS");
+            this.btnANS.Name = "btnANS";
+            this.btnANS.UseVisualStyleBackColor = false;
+            this.btnANS.Click += new System.EventHandler(this.btnANS_Click);
+            // 
+            // btnPowerAny
+            // 
+            this.btnPowerAny.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.btnPowerAny, "btnPowerAny");
+            this.btnPowerAny.Name = "btnPowerAny";
+            this.btnPowerAny.UseVisualStyleBackColor = false;
+            this.btnPowerAny.Click += new System.EventHandler(this.btnPowerAny_Click);
+            // 
+            // btnMultiplication
+            // 
+            this.btnMultiplication.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.btnMultiplication, "btnMultiplication");
+            this.btnMultiplication.Name = "btnMultiplication";
+            this.btnMultiplication.UseVisualStyleBackColor = false;
+            this.btnMultiplication.Click += new System.EventHandler(this.btnMultiplication_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.btnDivision, "btnDivision");
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.UseVisualStyleBackColor = false;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // btnAddition
+            // 
+            this.btnAddition.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.btnAddition, "btnAddition");
+            this.btnAddition.Name = "btnAddition";
+            this.btnAddition.UseVisualStyleBackColor = false;
+            this.btnAddition.Click += new System.EventHandler(this.btnAddition_Click);
+            // 
+            // btnSubstraction
+            // 
+            this.btnSubstraction.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.btnSubstraction, "btnSubstraction");
+            this.btnSubstraction.Name = "btnSubstraction";
+            this.btnSubstraction.UseVisualStyleBackColor = false;
+            this.btnSubstraction.Click += new System.EventHandler(this.btnSubstraction_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // helpBtn
+            // 
+            this.helpBtn.BackColor = System.Drawing.Color.Teal;
+            resources.ApplyResources(this.helpBtn, "helpBtn");
+            this.helpBtn.Name = "helpBtn";
+            this.helpBtn.UseVisualStyleBackColor = false;
+            this.helpBtn.Click += new System.EventHandler(this.helpBtn_Click);
+            // 
+            // floatingPtBtn
+            // 
+            this.floatingPtBtn.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.floatingPtBtn, "floatingPtBtn");
+            this.floatingPtBtn.Name = "floatingPtBtn";
+            this.floatingPtBtn.UseVisualStyleBackColor = false;
+            this.floatingPtBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ansBtn
+            // 
+            this.ansBtn.BackColor = System.Drawing.SystemColors.Highlight;
+            resources.ApplyResources(this.ansBtn, "ansBtn");
+            this.ansBtn.Name = "ansBtn";
+            this.ansBtn.UseVisualStyleBackColor = false;
+            this.ansBtn.Click += new System.EventHandler(this.ansBtn_Click);
+            // 
+            // MyCalculatorMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.ansBtn);
+            this.Controls.Add(this.floatingPtBtn);
+            this.Controls.Add(this.helpBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSubstraction);
+            this.Controls.Add(this.btnAddition);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplication);
+            this.Controls.Add(this.btnPowerAny);
+            this.Controls.Add(this.btnANS);
+            this.Controls.Add(this.btnAC);
+            this.Controls.Add(this.btnDEL);
+            this.Controls.Add(this.btnFactorial);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btnSquareRoot);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MyCalculatorMain";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnSquareRoot;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnDEL;
+        private System.Windows.Forms.Button btnAC;
+        private System.Windows.Forms.Button btnANS;
+        private System.Windows.Forms.Button btnPowerAny;
+        private System.Windows.Forms.Button btnMultiplication;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnSubstraction;
+        private System.Windows.Forms.Button btnFactorial;
+        private System.Windows.Forms.Button btnAddition;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button helpBtn;
+        private System.Windows.Forms.Button floatingPtBtn;
+        private System.Windows.Forms.Button ansBtn;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MyCalculatorMain : Form
+    {
+        private double[] numbers;
+        private int howManyNums;
+        private char[] operations;
+        private int howManyOps;
+        private double enterNumber;
+        private double enterNumberBefore;
+        private double mul;
+        private bool isPoint;
+        public MyCalculatorMain()
+        {
+            InitializeComponent();
+            numbers = new double[2];
+            operations = new char[1];
+            mul = 1;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(0);
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(1);
+        }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(2);
+        }
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(3);
+        }
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(4);
+        }
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(5);
+        }
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(6);
+        }
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(7);
+        }
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(8);
+        }
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            UpdateEnterNumber(9);
+        }
+        private void UpdateEnterNumber(double num)
+        {
+            enterNumberBefore = enterNumber;
+            if (!isPoint)
+            {
+                enterNumber *= mul;
+                enterNumber += num;
+                mul *= 10;
+            }
+            else
+            {
+                enterNumber += num * mul;
+                mul /= 10;
+            }
+            textBox1.Text += num.ToString();
+        }
+        private void AddNumber(double number)
+        {
+            if (howManyNums >= numbers.Length)
+                ResizeItNums();
+            numbers[howManyNums] = number;
+            howManyNums++;
+            enterNumberBefore = 0;
+        }
+        private void ResizeItNums()
+        {
+            double[] result = new double[numbers.Length + 10];
+            for (int i = 0; i < numbers.Length; i++)
+                result[i] = numbers[i];
+            numbers = result;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btnDEL_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            char[]items = str.ToCharArray();
+            if (items[items.Length - 1] >= '0' &&
+                items[items.Length - 1] <= '9')
+                enterNumber -= enterNumberBefore;
+            else
+            {
+                if (howManyOps > 0)
+                    howManyOps--;
+            }
+            str = str.Remove(str.Length - 1);
+            textBox1.Text = str;
+        }
+        private void btnAC_Click(object sender, EventArgs e)
+        {
+            howManyNums = 0;
+            howManyOps = 0;
+            enterNumber = 0;
+            enterNumberBefore = 0;
+            textBox1.ResetText();
+        }
+        private void AddOperation(char op)
+        {
+            AddNumber(enterNumber);
+            if (operations[howManyOps] == '!' && operations[howManyOps] == '√')
+                howManyNums--;
+            enterNumber = 0;
+            enterNumberBefore = 0;
+            mul = 1;
+            isPoint = false;
+            if (howManyOps >= operations.Length)
+                ResizeItOps();
+            operations[howManyOps] = op;
+            howManyOps++;
+        }
+        private void ResizeItOps()
+        {
+            char[] result = new char[operations.Length + 2];
+            for (int i = 0; i < operations.Length; i++)
+                result[i] = operations[i];
+            operations = result;
+        }
+        private void btnSquareRoot_Click(object sender, EventArgs e)
+        {
+            AddOperation('√');
+            textBox1.Text += "√";
+        }
+        private void btnPowerAny_Click(object sender, EventArgs e)
+        {
+            AddOperation('^');
+            textBox1.Text += "^";
+        }
+        private void btnFactorial_Click(object sender, EventArgs e)
+        {
+            AddOperation('!');
+            textBox1.Text += "!";
+        }
+        private void btnMultiplication_Click(object sender, EventArgs e)
+        {
+            AddOperation('x');
+            textBox1.Text += "x";
+        }
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            AddOperation('/');
+            textBox1.Text += "÷";
+        }
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            AddOperation('+');
+            textBox1.Text += "+";
+        }
+        private void btnSubstraction_Click(object sender, EventArgs e)
+        {
+            AddOperation('-');
+            textBox1.Text += "-";
+        }
+        private void btnANS_Click(object sender, EventArgs e)
+        {
+            AddNumber(enterNumber);
+            enterNumber = 0;
+            enterNumberBefore = 0;
+            mul = 1;
+            isPoint = false;
+            answer = CalculateIt();
+            textBox1.Text += "=" + answer.ToString();
+            textBox1.Text += "\r\n";
+            howManyNums = 0;
+            howManyOps = 0;
+        }
+        private double CalculateIt()
+        {
+            for (int i = 0; i < howManyOps; i++)
+            {
+                if (operations[i] == '^')
+                {
+                    double temp = Math.Pow(numbers[i], numbers[i + 1]);
+                    RemoveNumbers(i);
+                    numbers[i] = temp;
+                    RemoveOperations(i);
+                }
+                else if (operations[i] == '√')
+                {
+                    double temp = Math.Sqrt(numbers[i]);
+                    numbers[i] = temp;
+                    RemoveOperations(i);
+                }
+                else if (operations[i] == '!')
+                {
+                    double temp = GetFactorial(numbers[i]);
+                    numbers[i] = temp;
+                    RemoveOperations(i);
+                }
+            }
+            for (int i = 0; i < howManyOps; i++)
+            {
+                if (operations[i] == 'x' || operations[i] == '/')
+                {
+                    double temp = 0;
+                    if (operations[i] == 'x')
+                    {
+                        temp = numbers[i] * numbers[i + 1];
+                    }
+                    else
+                    {
+                        temp = numbers[i] / numbers[i + 1];
+                    }
+                    RemoveNumbers(i);
+                    numbers[i] = temp;
+                    RemoveOperations(i);
+                }
+            }
+            double sum = 0;
+            sum += numbers[0];
+            for (int i = 0; i < howManyOps; i++)
+            {
+                if (operations[i] == '+')
+                    sum += numbers[i + 1];
+                else
+                    sum -= numbers[i + 1];
+            }
+            return sum;
+        }
+        private double GetFactorial(double v)
+        {
+            double mul = 1;
+            while (v > 0)
+            {
+                mul *= v;
+                v--;
+            }
+            return mul;
+        }
+        private void RemoveOperations(int index)
+        {
+            for (int i = index; i < howManyOps - 1; i++)
+                operations[i] = operations[i + 1];
+            howManyOps--;
+        }
+        private void RemoveNumbers(int index)
+        {
+            for (int i = index; i < howManyNums - 1; i++)
+                numbers[i] = numbers[i + 1];
+            howManyNums--;
+        }
+        private void helpBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("DEL button - delete\n" +
+                "AC button - clear all\n" +
+                "ANS button - answer\n" +
+                "SQRT button - square root\n" +
+                "POW button - pow\n" +
+                "FACT button - factorial\n" +
+                "MUL button - multiplication\n" +
+                "DIV button - division\n" +
+                "ADD button - addition\n" +
+                "SUB button - substraction\n", "HELP DOCUMENTATION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void button1_Click(object sender, EventArgs e) // floatingPtBtn_Click
+        {
+            textBox1.Text += '.';
+            isPoint = true;
+            mul = 1 / (double)10;
+        }
+        private double answer;
+        private void ansBtn_Click(object sender, EventArgs e)
+        {
+            enterNumberBefore = enterNumber;
+            enterNumber = 0;
+            UpdateEnterNumber(answer);
+            answer = 0;
+        }
+    }
+}
